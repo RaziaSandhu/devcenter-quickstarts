58f263c54f23aa72664c18563c4febdc9ab9ec03
Ergin008 <ergin.dervisoglu@docusign.com>
2016-03-21 16:26:46 -0700

    Update SignatureRequest.php

--- 1-SignatureRequest/SignatureRequest.php
+++ 1-SignatureRequest/SignatureRequest.php
@@ -1,57 +1,104 @@
 <?php
 
-// 
-// DocuSign API Quickstart - Signature Request
-// 
+    require_once('./docusign-php-client/autoload.php');
 
-// Download PHP client:  https://github.com/docusign/DocuSign-PHP-Client
-require_once './DocuSign-PHP-Client/src/DocuSign_Client.php';
-require_once './DocuSign-PHP-Client/src/service/DocuSign_RequestSignatureService.php';
+    // DocuSign account credentials & Integrator Key
+    $username = "[USERNAME]";
+    $password = "[PASSWORD]";
+    $integrator_key = "[INTEGRATOR_KEY]";
+    $host = "https://demo.docusign.net/restapi";
 
-//=======================================================================================================================
-// STEP 1: Login API 
-//=======================================================================================================================
+    // create a new DocuSign configuration and assign host and header(s)
+    $config = new DocuSign\eSign\Configuration();
+    $config->setHost($host);
+    $config->addDefaultHeader("X-DocuSign-Authentication", "{\"Username\":\"" . $username . "\",\"Password\":\"" . $password . "\",\"IntegratorKey\":\"" . $integrator_key . "\"}");
 
-// client configuration
-$testConfig = array(
-	// Enter your Integrator Key, Email, and Password
-	'integrator_key' => "INTEGRATOR_KEY", 'email' => "EMAIL", 'password' => "PASSWORD",
-	// API version and environment (demo, www, etc)
-	'version' => 'v2', 'environment' => 'demo'
-);
+    /////////////////////////////////////////////////////////////////////////
+    // STEP 1:  Login() API
+    /////////////////////////////////////////////////////////////////////////
 
-// instantiate client object and call Login API
-$client = new DocuSign_Client($testConfig);
+    // instantiate a new docusign api client
+    $apiClient = new DocuSign\eSign\ApiClient($config);
 
-if( $client->hasError() )
-{
-	echo "\nError encountered in client, error is: " . $client->getErrorMessage() . "\n";
-	return;
-}
+    // we will first make the Login() call which exists in the AuthenticationApi...
+    $authenticationApi = new DocuSign\eSign\Api\AuthenticationApi($apiClient);
 
-//=======================================================================================================================
-// STEP 2: Create and Send Envelope API 
-//=======================================================================================================================
+    // optional login parameters
+    $options = new \DocuSign\eSign\Api\AuthenticationApi\LoginOptions();
 
-$service = new DocuSign_RequestSignatureService($client);
+    // call the login() API
+    $loginInformation = $authenticationApi->login($options);
 
-// Configure envelope settings, document(s), and recipient(s)
-$emailSubject = "Please sign my document";
-$emailBlurb = "This goes in the email body";	
-// create one signHere tab for the recipient
-$tabs = array( "signHereTabs" => array( 
-	array( "documentId"=>"1","pageNumber" => "1","xPosition" => "100","yPosition" => "150" )));
-$recipients = array( new DocuSign_Recipient( "1", "1", "RECIPIENT_NAME", "RECIPIENT_EMAIL", NULL, 'signers', $tabs));
-$documents = array( new DocuSign_Document("TEST.PDF", "1", file_get_contents("/PATH/TO/DOCUMENT/TEST.PDF") ));
+    // parse the login results
+    if(isset($loginInformation) && count($loginInformation) > 0)
+    {
+        // note: defaulting to first account found, user might be a 
+        // member of multiple accounts
+        $loginAccount = $loginInformation->getLoginAccounts()[0];
+        if(isset($loginInformation))
+        {
+            $accountId = $loginAccount->getAccountId();
+            if(!empty($accountId))
+            {
+                echo "Account ID = $accountId\n";
+            }
+        }
+    }
 
-// "sent" to send immediately, "created" to save as draft in your account	
-$status = 'sent'; 
+    /////////////////////////////////////////////////////////////////////////
+    // STEP 2:  Create & Send Envelope (aka Signature Request)
+    /////////////////////////////////////////////////////////////////////////
 
-//*** Send the signature request!
-$response = $service->signature->createEnvelopeFromDocument( 
-	$emailSubject, $emailBlurb, $status, $documents, $recipients, array() );
+    // set recipient information
+    $recipientName = "[RECIPIENT_NAME]";
+    $recipientEmail = "[RECIPIENT_EMAIL]";
 
-echo "\n-- Results --\n\n";
-print_r($response);
+    // configure the document we want signed
+    $documentFileName = "[PATH/TO/DOCUMENT.PDF]";
+    $documentName = "SignTest1.pdf";
+
+    // instantiate a new envelopeApi object
+    $envelopeApi = new DocuSign\eSign\Api\EnvelopesApi($apiClient);
+
+    // Add a document to the envelope
+    $document = new DocuSign\eSign\Model\Document();
+    $document->setDocumentBase64(base64_encode(file_get_contents(__DIR__ . $documentFileName)));
+    $document->setName($documentName);
+    $document->setDocumentId("1");
+
+    // Create a |SignHere| tab somewhere on the document for the recipient to sign
+    $signHere = new \DocuSign\eSign\Model\SignHere();
+    $signHere->setXPosition("100");
+    $signHere->setYPosition("100");
+    $signHere->setDocumentId("1");
+    $signHere->setPageNumber("1");
+    $signHere->setRecipientId("1");
+
+    // add the signature tab to the envelope's list of tabs
+    $tabs = new DocuSign\eSign\Model\Tabs();
+    $tabs->setSignHereTabs(array($signHere));
+
+    // add a signer to the envelope
+    $signer = new \DocuSign\eSign\Model\Signer();
+    $signer->setEmail($recipientEmail);
+    $signer->setName($recipientName);
+    $signer->setRecipientId("1");
+    $signer->setTabs($tabs);
+
+    // Add a recipient to sign the document
+    $recipients = new DocuSign\eSign\Model\Recipients();
+    $recipients->setSigners(array($signer));
+    $envelop_definition = new DocuSign\eSign\Model\EnvelopeDefinition();
+    $envelop_definition->setEmailSubject("[DocuSign PHP SDK] - Please sign this doc");
+
+    // set envelope status to "sent" to immediately send the signature request
+    $envelop_definition->setStatus("sent");
+    $envelop_definition->setRecipients($recipients);
+    $envelop_definition->setDocuments(array($document));
+
+    // create and send the envelope! (aka signature request)
+    $envelop_summary = $envelopeApi->createEnvelope($accountId, $envelop_definition, null);
+
+    echo "$envelop_summary\n";
 
 ?>
